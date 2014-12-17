6d3872324440b369bdb0c448551c247c3c3c0aa3
Florian Schlachter <flori@n-schlachter.de>
2014-12-17 11:12:06 +0100

    Create consts.go

---
+++ route/consts.go
@@ -0,0 +1,10 @@
+package main
+
+const (
+	// Standardgeschwindigkeiten in km/h
+	STANDARD_CAR_SPEED  = 45.0
+	STANDARD_BIKE_SPEED = 15.0
+
+	TRAFFIC_LIGHT_WAIT_TIME = 25 // in seconds
+	LEVEL_CROSSING_TIME     = 5  // in seconds
+)
